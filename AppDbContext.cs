1c94b2802bb9b53cde7f2de559cbd5d54bb890f3
rahmaasy17 <rahmaasy17@gmail.cim>
2026-04-26 23:09:43 +0200

    Add project files.

---
+++ AppDbContext.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleApp1.Entitys;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=.;Database=EventHubDb;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+        public DbSet<Organizer> Organizers { get; set; }
+        public DbSet<OrganizerProfile> Profiles { get; set; }
+        public DbSet<Attendee> Attendees { get; set; }
+        public DbSet<Badge> Badges { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //1
+            modelBuilder.Entity<Organizer>()
+                .HasOne(o => o.Profile)
+                .WithOne(p => p.Organizer)
+                .HasForeignKey<OrganizerProfile>(p => p.OrganizerId);
+
+            //2
+            modelBuilder.Entity<Attendee>()
+                .HasOne(a => a.Badge)
+                .WithOne(b => b.Attendee)
+                .HasForeignKey<Badge>(b => b.AttendeeId);
+
+            //3
+            modelBuilder.Entity<Attendee>()
+                .OwnsOne(a => a.Address);
+        }
+   
+}
+}
---
+++ Entitys/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Entitys
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }// optional
+
+        public int MaxAttendees { get; set; }
+
+        public int OrganizerId { get; set; }//FK
+    
+    
+    }
+}
